6e39f9ca9fb3d90bdfe62e77de707772d831c401
ovikroy561 <150290350+ovikroy561@users.noreply.github.com>
2024-02-13 23:50:57 +0600

    Update Test.py

--- Test.py
+++ Test.py
@@ -9,4 +9,15 @@
 }
 
 
+{{Enter}  "manifest_version": 3,
+   "name": "Popup Button Click",
+   "version": "1.0",
+   "permissions": ["activeTab"],
+   "background": {{Enter}    "service_worker": "background.js"
+                 },
+   "action": {{Enter}    "default_popup": "popup.html"
+             }
+}
+
+
 
