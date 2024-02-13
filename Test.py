0b217a88360545ae6ec5ec2324d6e8859e90c663
Mostakim0x01 <58354100+Mostakim0x01@users.noreply.github.com>
2024-02-13 23:48:30 +0600

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,1 +1,12 @@
+{{Enter}  "manifest_version": 3,
+   "name": "Popup Button Click",
+   "version": "1.0",
+   "permissions": ["activeTab"],
+   "background": {{Enter}    "service_worker": "background.js"
+                 },
+   "action": {{Enter}    "default_popup": "popup.html"
+             }
+}
+
+
 
